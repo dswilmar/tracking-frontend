dd25526f90d81d9c99c6be6686618b9b89e7c038
Wilmar dos Santos <dswilmar@gmail.com>
2023-06-08 08:52:37 -0300

    feat: formulario para tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
 import './App.css'
 
 function App() {
 
+  const [trackingCode, setTrackingCode] = useState<string>('');
+
+  const handleInputTracking = (e: ChangeEvent<HTMLInputElement>) => {
+    setTrackingCode(e.target.value);
+  }
+
+  const handleSubmitTracking = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(`Rastreamento: ${trackingCode}`);
+  }
+
   return (
     <div>
-      <h2>Tracking Frontend</h2>
+      <h2>Rastreamento de encomendas</h2>
+      <form onSubmit={handleSubmitTracking}>
+        <input type="text" style={{
+          marginRight: 10,
+          padding: 10
+        }}
+        onChange={handleInputTracking}
+          />
+        <button type="submit">Rastrear</button>        
+      </form>
     </div>
   )
 }
